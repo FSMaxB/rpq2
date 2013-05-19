7deacb1551c528dfab2ec381f80e8daeca1b1e16
Max Bruckner <max@maxbruckner.de>
2013-05-19 17:46:14 +0200

    C++-Bestandteil empfängt Daten

--- nativ/compile.sh
+++ nativ/compile.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-g++ einstell.cpp -o einstell -lpthread -lm;
-echo einstell kompiliert;
-g++ test.cpp -o test;
-echo test kompiliert;
+g++ einstell.cpp -o einstell -lpthread -lm
+echo einstell kompiliert
+g++ test.cpp -o test
+echo test kompiliert
+g++ mess.cpp
+echo mess kompiliert
