afb4982feafde17c2c8282e175b9fd4905d83a01
Max Bruckner <max@maxbruckner.de>
2013-05-19 21:38:37 +0200

    Linker-Direktiven in compile.sh vergessen

--- nativ/compile.sh
+++ nativ/compile.sh
@@ -3,5 +3,5 @@ g++ einstell.cpp -o einstell -lpthread -lm
 echo einstell kompiliert
 g++ test.cpp -o test
 echo test kompiliert
-g++ mess.cpp
+g++ mess.cpp -o mess -lm -lpthread
 echo mess kompiliert
