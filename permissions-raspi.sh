cdca0038ff2d0b0ac527e1c3ae8b393cd7413890
Max Bruckner <max@maxbruckner.de>
2013-08-18 16:01:57 +0200

    Bugfix: root.sh wurden Rechte genommen

--- permissions-raspi.sh
+++ permissions-raspi.sh
@@ -7,4 +7,5 @@ find . -type d -exec chmod 755 {} \;
 chmod 777 docs einstell-mess log misc pdo wartung
 chmod 666 docs/* einstell-mess/* log/* misc/* pdo/* wartung/*
 chmod a+x root.sh nativ/compile.sh nativ/einstell nativ/mess nativ/shutdown-timer nativ/test
+chown root:root root.sh
 chmod +s root.sh
