1c0303ab18bc27299fc6936282f353271883fed1
Max Bruckner <max@maxbruckner.de>
2013-08-18 15:57:21 +0200

    Bugfix: Permission-Skript

--- permissions-raspi.sh
+++ permissions-raspi.sh
@@ -2,8 +2,8 @@
 USER=pi
 GROUP=pi
 chown -R $USER:$GROUP .
-find . -type f -exec chmod {} 644 \;
-find . -type d -exec chmod {} 755 \;
+find . -type f -exec chmod 644 {} \;
+find . -type d -exec chmod 755 {} \;
 chmod 777 docs einstell-mess log misc pdo wartung
 chmod 666 docs/* einstell-mess/* log/* misc/* pdo/* wartung/*
 chmod a+x root.sh nativ/compile.sh nativ/einstell nativ/mess nativ/shutdown-timer nativ/test
