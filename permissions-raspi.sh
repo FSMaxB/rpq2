b86f3e0fff33d98742211ea07a9c1615747aa1b5
Max Bruckner <max@maxbruckner.de>
2013-08-18 16:31:21 +0200

    Bugfix: Setuid-Problem behoben

--- permissions-raspi.sh
+++ permissions-raspi.sh
@@ -7,5 +7,5 @@ find . -type d -exec chmod 755 {} \;
 chmod 777 docs einstell-mess log misc pdo wartung
 chmod 666 docs/* einstell-mess/* log/* misc/* pdo/* wartung/*
 chmod a+x root.sh nativ/compile.sh nativ/einstell nativ/mess nativ/shutdown-timer nativ/test
-chown root:root root.sh
+chown root:root root
 chmod +s root.sh
--- root.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#Dieses Skript muss den Eigentümer und die Gruppe root haben und mit dem Setuid-Bit versehen sein
-sh permissions-raspi.sh
