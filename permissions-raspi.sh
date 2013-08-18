9322efc3be09a48a290ac3e974e4f630d4340050
Max Bruckner <max@maxbruckner.de>
2013-08-18 14:24:08 +0200

    Permission-Script für Raspberry-Pi

---
+++ permissions-raspi.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+USER=pi
+GROUP=pi
+chown -R $USER:$GROUP .
+find . -type f -exec chmod {} 644 \;
+find . -type d -exec chmod {} 755 \;
+chmod 777 docs einstell-mess log misc pdo wartung
+chmod 666 docs/* einstell-mess/* log/* misc/* pdo/* wartung/*
+chmod a+x root.sh nativ/compile.sh nativ/einstell nativ/mess nativ/shutdown-timer nativ/test
+chmod +s root.sh
