ddda9f127715cce1b1ef86ee8cf6193e1c350b9f
Max Bruckner <max@maxbruckner.de>
2014-02-02 21:35:28 +0100

    Forwarding: SSH-Konfiguration erklärt

--- forwarding.sh
+++ forwarding.sh
@@ -2,6 +2,16 @@
 
 # Dieses Skript dient dem Weiterleiten des Webinterfaces per SSH-Forwarding zu einem anderen Computer
 
+# Damit die Portweiterleitung funktioniert, müssen folgende Einstellungen gesetzt sein:
+# SERVER (in /etc/ssh/sshd_config):
+# PubkeyAuthentication yes
+# PasswordAuthentication no #Achtung, hier kann man sich leicht selbst aussperren
+#
+# CLIENT also der Rechner mit Webinterface ( in ~/.ssh/config [für den User des Webservers] oder /etc/ssh/ssh_config):
+# BatchMode yes
+# ServerAliveInterval 10
+# ServerAliveCountMax 3
+
 if [ $1 ]
 then
     USER=$1
@@ -14,6 +24,6 @@ then
         ssh -N -R $HOST:8080:localhost:80 $USER@$HOST
     fi
 else
-    echo "Benutzung: $0 nutzer zielserver private-key"
+    echo "Benutzung: $0 nutzer zielserver [private-key]"
 fi
 
