12154eae0cc2fff9e308b80ef5c9730ed6053ac0
Max Bruckner <max@maxbruckner.de>
2014-02-02 16:57:50 +0100

    Forwarding: Privater Schlüssel muss angegeben werden

--- forwarding.sh
+++ forwarding.sh
@@ -6,8 +6,9 @@ if [ $1 ]
 then
     USER=$1
     HOST=$2
-    ssh -i .ssh/id-rsa -R $HOST:8080:localhost:80 $USER@$HOST
+    IDENTITY=$3
+    ssh -i $IDENTITY -R $HOST:8080:localhost:80 $USER@$HOST
 else
-    echo "Benutzung: $0 nutzer zielserver"
+    echo "Benutzung: $0 nutzer zielserver private-key"
 fi
 
