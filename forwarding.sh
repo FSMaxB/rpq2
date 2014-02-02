e08638c874b51186945845edc7f14295d3eb70c2
Max Bruckner <max@maxbruckner.de>
2014-02-02 21:22:18 +0100

    Forwarding: Identity-Option ist nun optional

--- forwarding.sh
+++ forwarding.sh
@@ -6,8 +6,13 @@ if [ $1 ]
 then
     USER=$1
     HOST=$2
-    IDENTITY=$3
-    ssh -N -i $IDENTITY -R $HOST:8080:localhost:80 $USER@$HOST
+    if [ $3 ]
+    then
+        IDENTITY=$3
+        ssh -N -i $IDENTITY -R $HOST:8080:localhost:80 $USER@$HOST
+    else
+        ssh -N -R $HOST:8080:localhost:80 $USER@$HOST
+    fi
 else
     echo "Benutzung: $0 nutzer zielserver private-key"
 fi
